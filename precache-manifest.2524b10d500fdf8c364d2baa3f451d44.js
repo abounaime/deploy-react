349c9d745688b605a9364079b9e829618df2e87c
Bounaime <a.bounaime@gmail.com>
2019-05-01 23:22:50 +0200

    Updates

---
+++ precache-manifest.2524b10d500fdf8c364d2baa3f451d44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70ab81c67defc9fa7dc",
+    "url": "/deploy-react/static/js/runtime~main.5de6419e.js"
+  },
+  {
+    "revision": "ebc2c9538d302ba1d020",
+    "url": "/deploy-react/static/js/main.095b507d.chunk.js"
+  },
+  {
+    "revision": "f63b3884f2c37c8ee5d7",
+    "url": "/deploy-react/static/js/2.d1a78710.chunk.js"
+  },
+  {
+    "revision": "ebc2c9538d302ba1d020",
+    "url": "/deploy-react/static/css/main.d144602d.chunk.css"
+  },
+  {
+    "revision": "f63b3884f2c37c8ee5d7",
+    "url": "/deploy-react/static/css/2.1f86085a.chunk.css"
+  },
+  {
+    "revision": "74835978f0bd2a2d520a7cd1b99aea1e",
+    "url": "/deploy-react/index.html"
+  }
+];
