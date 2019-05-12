43f0985f36e8d85829fd1a3cac362a6169484f92
Bounaime <a.bounaime@gmail.com>
2019-05-12 23:07:49 +0200

    Updates

---
+++ precache-manifest.548f5c21dc5cbb1c618c05a2099e8a13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70ab81c67defc9fa7dc",
+    "url": "/deploy-react/static/js/runtime~main.5de6419e.js"
+  },
+  {
+    "revision": "6d234466e4844f57a326",
+    "url": "/deploy-react/static/js/main.2d1db277.chunk.js"
+  },
+  {
+    "revision": "fe47a4aa29932a96f65a",
+    "url": "/deploy-react/static/js/2.d1a78710.chunk.js"
+  },
+  {
+    "revision": "6d234466e4844f57a326",
+    "url": "/deploy-react/static/css/main.08485d58.chunk.css"
+  },
+  {
+    "revision": "fe47a4aa29932a96f65a",
+    "url": "/deploy-react/static/css/2.02f55e68.chunk.css"
+  },
+  {
+    "revision": "86db4ef2280e59ed8c9625f9f9e3b5c1",
+    "url": "/deploy-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/deploy-react/precache-manifest.cd787536da77b4d9885f754b7ae146f1.js"
+  "/deploy-react/precache-manifest.548f5c21dc5cbb1c618c05a2099e8a13.js"
 );
 
 workbox.clientsClaim();
